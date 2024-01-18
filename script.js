1867fd33c4ec9645c8671e27fd5b7c2bce4b33fd
Aashish Gautam <ashishgautam7.ag@gmail.com>
2024-01-17 19:19:08 -0500

    addes responsive desine

--- script.js
+++ script.js
@@ -1,37 +1,41 @@
-const navBar = document.getElementById('navbar');
-let lastScrollPos = 0
+const navBar = document.getElementById("navbar");
+let lastScrollPos = 0;
 
-window.addEventListener('scroll', function(){
-    let currentScrollPos = window.scrollY;
-    if (currentScrollPos>lastScrollPos) {
-        navBar.classList.add('white')
-    }else{
-        navBar.classList.remove('white')
-    }
-})
-const serviceDiv = document.querySelectorAll('.idobox')
-serviceDiv.forEach(div=>{
-    const heding = div.querySelector('.heading')
+window.addEventListener("scroll", function () {
+  let currentScrollPos = window.scrollY;
+  if (currentScrollPos > lastScrollPos) {
+    navBar.classList.add("white");
+  } else {
+    navBar.classList.remove("white");
+  }
+});
+const serviceDiv = document.querySelectorAll(".idobox");
+serviceDiv.forEach((div) => {
+  const heding = div.querySelector(".heading");
 
-    div.addEventListener('mouseenter', function(){
-        heding.classList.add("hidden")
-       
-        
-    })
-    div.addEventListener('mouseleave', function(){
-        setTimeout(() => {
-            heding.classList.remove("hidden")
-        }, 356);
-        
-    })
-})
+  div.addEventListener("mouseenter", function () {
+    heding.classList.add("hidden");
+  });
+  div.addEventListener("mouseleave", function () {
+    setTimeout(() => {
+      heding.classList.remove("hidden");
+    }, 356);
+  });
+});
 function cvButton() {
-    console.log("pressed");
-    var filePath = "asset/Aashish Gautam Resume .docx";
-    const a = document.createElement('a');
-    a.href = filePath;
-    a.download = "Aashish's Resume"
-    a.click();
-    alert('Resume downloaded')
+  console.log("pressed");
+  var filePath = "asset/Aashish Gautam Resume .docx";
+  const a = document.createElement("a");
+  a.href = filePath;
+  a.download = "Aashish's Resume";
+  a.click();
+  alert("Resume downloaded");
 }
-
+function phoneLogo() {
+  const mainLeftHead = document.getElementById("leftnavlogo");
+  var screensize = window.innerWidth;
+  if (screensize <= 800) {
+    mainLeftHead.textContent = "AG";
+  }
+}
+phoneLogo();
